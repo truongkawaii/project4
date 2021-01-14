0fa30a27024e4bd6fba85e2d6dd93a9ad0a0496c
intern_truong <anhviphg02@gmail.com>
2021-01-14 17:58:22 +0700

    v

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,28 +1,25 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+// import { ToastContainer } from "react-toastify";
 import Login from "../../container/Login";
-import { addTodo } from "../../state/actions";
-import 'antd/dist/antd.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "antd/dist/antd.css";
 import "./App.css";
 
 function App() {
-  const todoList = useSelector((state: any) => state.todoList.num);
-  const dispatch = useDispatch();
-
-  const handlerAddTodo = () => {
-    dispatch(addTodo());
-  };
-
   return (
-    <div className="App">
-        <ToastContainer />
-      <input type="text" placeholder="input" />
-      <button onClick={handlerAddTodo}>CLICK</button>
-      <h2>{todoList}</h2>
-      <Login/>
-    </div>
+   
+    <Router>
+      <div className="">
+        <h2>HI</h2>
+      </div>
+      <Switch>
+        <Route path="/login" exact>
+          <Login />
+        </Route>
+      </Switch>
+      {/* <ToastContainer /> */}
+    </Router>
   );
 }
 
--- client/src/constant/constant.ts
+++ client/src/constant/constant.ts
@@ -1,2 +1,3 @@
 export const a = 5;
+export const BASE_URL = 'https://localhost:5001';
  
